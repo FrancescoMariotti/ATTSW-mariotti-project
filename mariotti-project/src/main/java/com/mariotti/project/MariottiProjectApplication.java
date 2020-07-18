d84c74a39c5226099b802c3abe73b4cd36b0924c
francesco <mariotti.fra96@gmail.com>
2020-07-18 15:12:41 +0200

    created project

---
+++ mariotti-project/src/main/java/com/mariotti/project/MariottiProjectApplication.java
@@ -0,0 +1,13 @@
+package com.mariotti.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MariottiProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MariottiProjectApplication.class, args);
+	}
+
+}
---
+++ mariotti-project/src/test/java/com/mariotti/project/MariottiProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mariotti.project;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MariottiProjectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
