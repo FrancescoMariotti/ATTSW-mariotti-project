f8e2271b12171a02d7cda98ac52244d9f24b2574
francesco <mariotti.fra96@gmail.com>
2020-07-21 11:22:46 +0200

    implemented JPA mapping tests

---
+++ mariotti-project/src/test/java/com/mariotti/project/jpa/OfficeJpaTest.java
@@ -0,0 +1,31 @@
+package com.mariotti.project.jpa;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.mariotti.project.models.Employee;
+import com.mariotti.project.models.Office;
+
+@DataJpaTest
+@RunWith(SpringRunner.class)
+public class OfficeJpaTest {
+	@Autowired
+	private TestEntityManager entityManager;
+
+	@Test
+	public void testJpaMapping() {
+		Office saved = entityManager.persistFlushFind(new Office(null, "office", new ArrayList<Employee>()));
+		assertThat(saved.getName()).isEqualTo("office");
+		assertThat(saved.getEmployees()).isNotNull();
+		assertThat(saved.getId()).isNotNull();
+		assertThat(saved.getId()).isGreaterThan(0);
+	}
+}
