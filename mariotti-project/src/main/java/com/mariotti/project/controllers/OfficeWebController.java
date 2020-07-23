ceee6d71a245bcd94f5a98ab6af30bbc857c0491
francesco <mariotti.fra96@gmail.com>
2020-07-23 11:53:16 +0200

    test succeeded with mapping and addition of index.html

--- mariotti-project/src/main/java/com/mariotti/project/controllers/OfficeWebController.java
+++ mariotti-project/src/main/java/com/mariotti/project/controllers/OfficeWebController.java
@@ -1,8 +1,14 @@
 package com.mariotti.project.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class OfficeWebController {
-
+	
+	@GetMapping("/")
+	public String index() {
+		return "index";
+	}
+	
 }
