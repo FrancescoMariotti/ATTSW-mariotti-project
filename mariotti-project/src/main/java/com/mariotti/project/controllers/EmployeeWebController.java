7bdc985d79d5e9086d20d8bccb164410f0236cd4
francesco <mariotti.fra96@gmail.com>
2020-07-26 16:00:50 +0200

    test succeeded

--- mariotti-project/src/main/java/com/mariotti/project/controllers/EmployeeWebController.java
+++ mariotti-project/src/main/java/com/mariotti/project/controllers/EmployeeWebController.java
@@ -1,8 +1,17 @@
 package com.mariotti.project.controllers;
 
+
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 @Controller
 public class EmployeeWebController {
 
+	@GetMapping("/employees_office/{officeId}")
+	public String index(Model model) {
+		return "employees_office";
+	}
 }
