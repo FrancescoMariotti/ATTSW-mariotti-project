982d62533f75916940f0792fd24e6ab74deca35c
francesco <mariotti.fra96@gmail.com>
2020-07-26 16:15:49 +0200

    added office attribute

--- mariotti-project/src/main/java/com/mariotti/project/controllers/EmployeeWebController.java
+++ mariotti-project/src/main/java/com/mariotti/project/controllers/EmployeeWebController.java
@@ -2,16 +2,26 @@ package com.mariotti.project.controllers;
 
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.mariotti.project.models.Office;
+import com.mariotti.project.services.OfficeService;
 
 
 @Controller
 public class EmployeeWebController {
 
+	@Autowired
+	private OfficeService officeService;
+
 	@GetMapping("/employees_office/{officeId}")
-	public String index(Model model) {
+	public String index(@PathVariable long officeId, Model model) {
+		Office officeById = officeService.getOfficeById(officeId);
+		model.addAttribute("office", officeById);
 		return "employees_office";
 	}
 }
