b6ae572eb4614b7895bcace6ebb61cd04d000ba5
francesco <mariotti.fra96@gmail.com>
2020-07-28 17:55:26 +0200

    failing test for editing existent employee

--- mariotti-project/src/test/java/com/mariotti/project/controllers/EmployeeWebControllerHtmlTest.java
+++ mariotti-project/src/test/java/com/mariotti/project/controllers/EmployeeWebControllerHtmlTest.java
@@ -2,6 +2,7 @@ package com.mariotti.project.controllers;
 
 import static java.util.Arrays.asList;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
@@ -15,6 +16,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlForm;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import com.gargoylesoftware.htmlunit.html.HtmlTable;
 import com.mariotti.project.models.Employee;
@@ -67,6 +69,19 @@ public class EmployeeWebControllerHtmlTest {
 		when(employeeService.getEmployeeById(1L)).thenReturn(null);
 		HtmlPage page = this.webClient.getPage("/employees_office/1/edit_employee/1");
 		assertThat(page.getBody().getTextContent()).contains("No employee found with id: 1");
+	}
+	
+	@Test
+	public void testEditExistentEmployee() throws Exception {
+		Office office = new Office(1L, "office", new ArrayList<Employee>());
+		when(officeService.getOfficeById(1L)).thenReturn(office);
+		when(employeeService.getEmployeeById(1)).thenReturn(new Employee(1L, "original name", 1000, office));
+		HtmlPage page = this.webClient.getPage("/employees_office/1/edit_employee/1");
+		final HtmlForm form = page.getFormByName("employee_form");
+		form.getInputByValue("original name").setValueAttribute("modified name");
+		form.getInputByValue("1000").setValueAttribute("3000");
+		form.getButtonByName("btn_submit").click();
+		verify(employeeService).updateEmployeeById(1L, new Employee(1L, "modified name", 3000, office));
 	}
 	
 	private String removeWindowsCR(String s) {
