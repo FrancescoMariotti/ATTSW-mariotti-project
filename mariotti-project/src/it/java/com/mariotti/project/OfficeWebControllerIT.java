5a548625f89e0f34bda6aab417ffe88a4e67bb19
francesco <mariotti.fra96@gmail.com>
2020-08-05 11:09:33 +0200

    IT for OfficeWebController

---
+++ mariotti-project/src/it/java/com/mariotti/project/OfficeWebControllerIT.java
@@ -0,0 +1,77 @@
+package com.mariotti.project;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.ArrayList;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+
+import com.mariotti.project.models.Office;
+import com.mariotti.project.repositories.OfficeRepository;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class OfficeWebControllerIT {
+
+	@LocalServerPort
+	private int port;
+	private WebDriver driver;
+	private String url;
+	@Autowired
+	private OfficeRepository officeRepository;
+
+	@Before
+	public void setup() {
+		officeRepository.deleteAll();
+		url = "http://localhost:" + port;
+		driver = new HtmlUnitDriver();
+	}
+
+	@After
+	public void teardown() {
+		driver.quit();
+	}
+
+	@Test
+	public void testHomePageShouldContainOffice() {
+		Office office = officeRepository.save(new Office(null, "office", new ArrayList<>()));
+		driver.get(url);
+		assertThat(driver.findElement(By.id("office_table")).getText()).contains("office", "Edit");
+		driver.findElement(By.cssSelector(
+				"a[href*='/edit_office/" + office.getId() + "']"));
+	}
+
+	@Test
+	public void testEditOfficePageNewOffice() throws Exception {
+		driver.get(url + "/new_office");
+		driver.findElement(By.name("name")).sendKeys("another office");
+		driver.findElement(By.name("btn_submit")).click();
+		assertThat(officeRepository.findByName("another office")).isNotNull();
+	}
+
+	@Test
+	public void testEditEmployeePageUpdateEmployee() throws Exception {
+		Office office = officeRepository.save(new Office(null, "old office", new ArrayList<>()));
+		driver.get(url + "/edit_office/" + office.getId());
+		final WebElement nameBox = driver.findElement(By.name("name"));
+		nameBox.clear();
+		nameBox.sendKeys("modified employee");
+		driver.findElement(By.name("btn_submit")).click();
+		assertThat(officeRepository.findByName("modified employee")).isNotNull();
+	}
+
+}
