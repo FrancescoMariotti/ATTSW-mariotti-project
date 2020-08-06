fe7da9dc2ddc4b5f68c874efdb56736c84676eb5
francesco <mariotti.fra96@gmail.com>
2020-08-06 11:22:45 +0200

    failing test for delete in EmployeeService

--- mariotti-project/src/test/java/com/mariotti/project/services/EmployeeServiceTest.java
+++ mariotti-project/src/test/java/com/mariotti/project/services/EmployeeServiceTest.java
@@ -28,25 +28,25 @@ public class EmployeeServiceTest {
 	private EmployeeRepository employeeRepository;
 
 	@Test
-	public void test_getAllEmployees() {
+	public void testGetAllEmployees() {
 		Employee firstEmployee = new Employee(1L, "first", 1000, new Office(1L, "office1", new ArrayList<>()));
-		Employee secondEmployee = new Employee(2L, "second", 1000,
-				new Office(2L, "office2", new ArrayList<>()));
+		Employee secondEmployee = new Employee(2L, "second", 1000, new Office(2L, "office2", new ArrayList<>()));
 		when(employeeRepository.findAll()).thenReturn(Arrays.asList(new Employee[] { firstEmployee, secondEmployee }));
 		assertThat(employeeService.getAllEmployees()).containsExactly(firstEmployee, secondEmployee);
 	}
 
 	@Test
-	public void test_getEmployeesByOffice() {
+	public void testGetEmployeesByOffice() {
 		Office office1 = new Office(1L, "office1", new ArrayList<>());
 		Employee employee1 = new Employee(1L, "first", 1000, office1);
 		Employee employee2 = new Employee(2L, "second", 1000, office1);
-		when(employeeRepository.findByOffice(office1)).thenReturn(Arrays.asList(new Employee[] { employee1, employee2 }));
+		when(employeeRepository.findByOffice(office1))
+				.thenReturn(Arrays.asList(new Employee[] { employee1, employee2 }));
 		assertThat(employeeService.getEmployeesByOffice(office1)).containsExactly(employee1, employee2);
 	}
 
 	@Test
-	public void test_getEmployeeById_notFound() {
+	public void testGetEmployeeById_notFound() {
 		when(employeeRepository.findById(anyLong())).thenReturn(Optional.empty());
 		assertThat(employeeService.getEmployeeById(1)).isNull();
 	}
@@ -59,7 +59,7 @@ public class EmployeeServiceTest {
 	}
 
 	@Test
-	public void test_insertNewEmployee() {
+	public void testInsertNewEmployee() {
 		Employee toSave = spy(new Employee(99L, "", 0, null));
 		Employee saved = new Employee(1L, "saved", 1000, new Office(1L, "office", new ArrayList<>()));
 		when(employeeRepository.save(any(Employee.class))).thenReturn(saved);
@@ -71,7 +71,7 @@ public class EmployeeServiceTest {
 	}
 
 	@Test
-	public void test_updateEmployeeById() {
+	public void testUpdateEmployeeById() {
 		Employee replacement = spy(new Employee(null, "employee", 0, null));
 		Employee replaced = new Employee(1L, "saved", 1000, new Office(1L, "office", new ArrayList<>()));
 		when(employeeRepository.save(any(Employee.class))).thenReturn(replaced);
@@ -80,5 +80,12 @@ public class EmployeeServiceTest {
 		InOrder inOrder = inOrder(replacement, employeeRepository);
 		inOrder.verify(replacement).setId(1L);
 		inOrder.verify(employeeRepository).save(replacement);
+	}
+
+	@Test
+	public void testDeleteEmployeeById() {
+		Employee employee = new Employee(1L, "employee", 1000, new Office(1L, "office", new ArrayList<>()));
+		employeeService.deleteEmployeeById(employee.getId());
+		verify(employeeRepository,times(1)).deleteById(employee.getId());
 	}
 }
