dfd696b82dbc8230519e0d82a110c8e477e4380d
francesco <mariotti.fra96@gmail.com>
2020-08-08 10:18:22 +0200

    implemented deletion of employees in office for OfficeService

--- mariotti-project/src/main/java/com/mariotti/project/services/OfficeService.java
+++ mariotti-project/src/main/java/com/mariotti/project/services/OfficeService.java
@@ -2,17 +2,24 @@ package com.mariotti.project.services;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.mariotti.project.models.Employee;
 import com.mariotti.project.models.Office;
+import com.mariotti.project.repositories.EmployeeRepository;
 import com.mariotti.project.repositories.OfficeRepository;
 
 @Service
 public class OfficeService {
 
-	@Autowired
 	private OfficeRepository officeRepository;
+
+	private EmployeeRepository employeeRepository;
+
+	public OfficeService(OfficeRepository officeRepository, EmployeeRepository employeeRepository) {
+		this.officeRepository = officeRepository;
+		this.employeeRepository = employeeRepository;
+	}
 
 	public List<Office> getAllOffices() {
 		return officeRepository.findAll();
@@ -33,6 +40,11 @@ public class OfficeService {
 	}
 
 	public void deleteOfficeById(long id) {
+		Office officeToDelete = officeRepository.findById(id).orElse(null);
+		List<Employee> employeesToDelete = employeeRepository.findByOffice(officeToDelete);
+		for (Employee employee : employeesToDelete) {
+			employeeRepository.deleteById(employee.getId());
+		}
 		officeRepository.deleteById(id);
 	}
 
