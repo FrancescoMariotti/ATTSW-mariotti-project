2304df678261dbc7d9b742891079c3e24d58c874
francesco <mariotti.fra96@gmail.com>
2020-08-19 16:37:45 +0200

    added Transactional annotation to *Service methods
    (insert, update, delete)

--- mariotti-project/src/main/java/com/mariotti/project/services/EmployeeService.java
+++ mariotti-project/src/main/java/com/mariotti/project/services/EmployeeService.java
@@ -46,5 +46,6 @@ public class EmployeeService {
 	public void deleteEmployeeById(long id) {
 		employeeRepository.deleteById(id);
 	}
+	
 
 }
--- mariotti-project/src/main/java/com/mariotti/project/services/OfficeService.java
+++ mariotti-project/src/main/java/com/mariotti/project/services/OfficeService.java
@@ -53,5 +53,6 @@ public class OfficeService {
 			officeRepository.deleteById(id);
 		}
 	}
+	
 
 }
